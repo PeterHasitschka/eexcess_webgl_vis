1cae32ea6c3d6e49108e2482c4b55d2525637787
Peter Hasitschka <hasitschka@student.tugraz.at>
2015-05-28 21:43:08 +0200

    DB-Handler: Loading db + getstorage fct

--- js/db/db_handler.js
+++ js/db/db_handler.js
@@ -10,16 +10,19 @@ GLVIS.DbHandler = function () {
 
 
 GLVIS.DbHandler.prototype.loadQueries = function (callback_ready) {
-
+    
+    var that = this;
     this.initDb_(function () {
 
         if (GLVIS.config.debug)
             console.log("DBHANDLER: Init DB ready.");
-        
-        
-        
-        
-        
+
+
+        that.getStorageData_(function (data) {
+            console.log(data);
+        }, "queries_full", ["id", "timestamp", "query"]);
+
+
         if (callback_ready)
             callback_ready();
     });
@@ -28,26 +31,73 @@ GLVIS.DbHandler.prototype.loadQueries = function (callback_ready) {
 };
 
 GLVIS.DbHandler.prototype.initDb_ = function (callback_ready) {
-
-    if (!callback_ready)
-        throw("No valid callback");
-
+    
     if (this.db_ !== null) {
         callback_ready();
         return;
     }
 
-    console.log("GO ON HERE AFTER INTEGRATION IN REC-DASHBOARD");
 
-
-    EEXCESS.storage.getDb(callback_ready, function () {
+    var that = this;
+    EEXCESS.storage.getDb(function (db) {
+        that.db_ = db;  //SUCCESS
+        callback_ready();
+    }, function () {    //ERROR
         throw("ERROR LOADING DB");
-    });
-
-
+    }
+    );
 
 };
 
+
+/**
+ * @param {function} cb_query_loaded Callback function
+ * @param {string} storage_name
+ * @param {array} fields keys of the columns to load
+ * @returns {array} holding all field names to be loaded
+ */
+GLVIS.DbHandler.prototype.getStorageData_ = function (cb_data_loaded, storage_name, fields) {
+
+
+    if (!this.db_)
+        throw ("DBHANDLER: ERROR: NO DB SET!");
+
+    var trans = this.db_.transaction(storage_name, 'readonly');
+    var store = trans.objectStore(storage_name);
+
+    var request = store.openCursor();
+
+    var data = [];
+
+    trans.oncomplete = function (evt) {
+        cb_data_loaded(data);
+    };
+
+    request.onsuccess = function (evt) {
+        var result = evt.target.result;
+        ;
+        var cursor = evt.target.result;
+
+        if (cursor) {
+
+            var data_element = {};
+
+            for (var i = 0; i < fields.length; i++)
+            {
+                data_element[fields[i]] = cursor.value[fields[i]];
+            }
+
+            data.push(data_element);
+            cursor.continue();
+        }
+    };
+
+    request.onerror = function (evt) {
+        console.log("ERROR on Request");
+    };
+
+
+};
 
 
 
