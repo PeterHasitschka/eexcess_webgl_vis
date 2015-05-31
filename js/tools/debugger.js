1233ff8d909f0362edbd06c95c2bbf2a27376902
Peter Hasitschka <hasitschka@student.tugraz.at>
2015-05-31 21:34:01 +0200

    colorful debug-fun :-)

--- js/tools/debugger.js
+++ js/tools/debugger.js
@@ -4,7 +4,9 @@ var GLVIS = GLVIS || {};
 GLVIS.Debugger = {
     active_: GLVIS.config.debug.active,
     level_: GLVIS.config.debug.level,
-    prefix_: GLVIS.config.debug.prefix
+    prefix_: GLVIS.config.debug.prefix,
+    css: "font-weight:bold;",
+    classname_css: "color:green;"
 };
 
 /**
@@ -18,6 +20,13 @@ GLVIS.Debugger.debug = function (class_name, value, level) {
 
     if (level > this.level_)
         return;
-    console.log(this.prefix_+"L"+level+"::'"+class_name+"':", value);
+    
+    
+    console.log(
+            this.prefix_ + "L" + level + "::'%c" + class_name + "':%c%s",
+            this.classname_css,
+            this.css,
+            value
+            );
 
 };
